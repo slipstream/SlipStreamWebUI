7729130a86a6813680df531d8907660ca51ef3bd
Charles Loomis <cal@sixsq.com>
2017-05-24 08:43:39 +0200

    add unit tests

--- test/clj/sixsq/slipstream/webui/run.clj
+++ test/clj/sixsq/slipstream/webui/run.clj
@@ -2,9 +2,9 @@
   (:require [ring.util.response :as r]))
 
 (defn index-handler
-  "For GET requests, always serves index.html from classpath.
-   If index.html cannot be found, then a 404 is returned. Any
-   method other than GET will return a 405 response."
+  "For GET requests, always serves index.html from classpath. If index.html
+   cannot be found, then a 404 is returned. Any method other than GET will
+   return a 405 response."
   [{:keys [request-method]}]
   (if (= request-method :get)
     (if-let [resp (r/resource-response "webui/index.html")]
