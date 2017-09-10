f7049e62e78409e68461663859727829c0fab973
Marc-Elian Bégin <meb@sixsq.com>
2017-09-10 22:35:17 +0200

    re-implemented accordeon with re-com

--- src/cljs/sixsq/slipstream/webui/widget/accordeon_menu/views.cljs
+++ src/cljs/sixsq/slipstream/webui/widget/accordeon_menu/views.cljs
@@ -11,17 +11,6 @@
 (defn sub-key [header sub]
   (str header "-" sub))
 
-(defn menu-background [display]
-  "Background of the menu when displayed in collapsed mode."
-  (when (= :show display)
-    [:div.accordeon-menu-background
-     {:on-click #(rf/dispatch [:main-menu/display :hide])
-      :style {:display          "block"
-              :position         "fixed"
-              :width            "100%"
-              :height           "100%"
-              :z-index          1}}]))
-
 (defn- display-dispatch-target-name [target]
   "Construct the name of the dispatch target. Use display since it sets the status of
    the menu display"
@@ -37,87 +26,71 @@
 (defn accordeon-menu [options & children]
   (let [{:keys [open-sections] :or {open-sections #{}}} options
         s (reagent/atom {:open-sections open-sections
-                         :last-header nil
-                         :last-sub nil})]
+                         :last-header   nil
+                         :last-sub      nil})]
     (fn [options & children]
       (let [{:keys [component-name]} options
             pairs (partition-all 2 children)
             i-h-s (map conj pairs (range))
             dispatch-target (display-dispatch-target-name component-name)
             display @(rf/subscribe [dispatch-target])]
-          [rc/v-box
-           :class "accordeon-menu"
-           :attr {:id component-name}
-           :children [
-                       ;[menu-background display]
-                       ;{:style {:position (if (= :show display) "fixed" "static")
-                       ;                             :left     "0px"
-                       ;                             :top      "0px"
-                       ;                             :width    "200px"
-                       ;                             :z-index  1020
-                       ;                             :height "100vh"}
-                       ;                     }
-                       [rc/box
-                        :class "accordeon-header"
-                        :child [:div.webui-logo {:style {:width  "20ex"
-                                                         :height "100%"}}]]
-                       (doall
-                         (for [[i header subsections] i-h-s]
-                           [rc/v-box
-                            :attr {:key (str component-name "-" i)}
-                            :children [[rc/h-box
-                                         :class (str "accordeon-menu-header"
-                                                  (when (= (:last-header @s) i)
-                                                    " accordeon-menu-header-selected"))
-                                         :justify :between
-                                         :attr {:on-click (fn []
-                                                            (swap! s update-in [:open-sections]
-                                                              #(if (contains? % i) (disj % i) (conj % i)))
-                                                            (swap! s update-in [:last-header]
-                                                              #(if (or (empty? (:open-sections @s))
-                                                                     (not (contains? (:open-sections @s) i)))
-                                                                 nil
-                                                                 (if (= % i) nil i))))}
-                                        :children [[:div header]
-                                                   (if (contains? (:open-sections @s) i)
-                                                     [:i.material-icons {:style {:cursor "default"}} "keyboard_arrow_down"]
-                                                     [:i.material-icons {:style {:cursor "default"}} "keyboard_arrow_right"])]]
-                             [rc/v-box
-                              :class "accordeon-menu-sub"
-                              :style {:height (if (contains? (:open-sections @s) i)
-                                                (when-let [el (get-in @s [:refs i])]
-                                                  (.-clientHeight el))
-                                                0)
-                                      :overflow :hidden
-                                      :transition "all 1s linear 0s"}
-                              :attr {:key (sub-key header i)}
-                              :children
-                              (doall (for [[sub j] (#(map list % (range)) subsections)]
-                                       [rc/box
-                                        :attr {:key (sub-key header sub)}
-                                        :child [:a
-                                                {:data-dispatch (:data-dispatch sub)
-                                                 :on-click (fn []
-                                                             (swap! s assoc-in [:last-sub] (sub-key header j))
-                                                             (rf/dispatch [(:data-dispatch sub)])
-                                                             (rf/dispatch [dispatch-target :hide]))}
-                                                [:div {:class (when (= (:last-sub @s) (sub-key header j))
-                                                                :accordeon-menu-sub-selected)}
-                                                 (:content sub)]]]))]]]))
-                      [gap]
-                      [rc/box
-                       :class "webui-footer"
-                       :child "Footer"]]]))))
-                       ;(pr-str @s)
+        [rc/v-box
+         :class "accordeon-menu"
+         :attr {:id component-name}
+         :children [
+                    [rc/box
+                     :class "accordeon-header"
+                     :child [:div.webui-logo {:style {:width  "20ex"
+                                                      :height "100%"}}]]
+                    (doall
+                      (for [[i header subsections] i-h-s]
+                        [rc/v-box
+                         :attr {:key (str component-name "-" i)}
+                         :children [[rc/h-box
+                                     :class (str "accordeon-menu-header"
+                                              (when (= (:last-header @s) i)
+                                                " accordeon-menu-header-selected"))
+                                     :justify :between
+                                     :attr {:on-click (fn []
+                                                        (swap! s update-in [:open-sections]
+                                                          #(if (contains? % i) (disj % i) (conj % i)))
+                                                        (swap! s update-in [:last-header]
+                                                          #(if (or (empty? (:open-sections @s))
+                                                                 (not (contains? (:open-sections @s) i)))
+                                                             nil
+                                                             (if (= % i) nil i))))}
+                                     :children [[:div header]
+                                                (if (contains? (:open-sections @s) i)
+                                                  [:i.material-icons {:style {:cursor "default"}} "keyboard_arrow_down"]
+                                                  [:i.material-icons {:style {:cursor "default"}} "keyboard_arrow_right"])]]
+                                    [rc/v-box
+                                     :class "accordeon-menu-sub"
+                                     :style {:height     (if (contains? (:open-sections @s) i)
+                                                           (when-let [el (get-in @s [:refs i])]
+                                                             (.-clientHeight el))
+                                                           0)
+                                             :overflow   :hidden
+                                             :transition "all 1s linear 0s"}
+                                     :attr {:key (sub-key header i)}
+                                     :children
+                                     (doall (for [[sub j] (#(map list % (range)) subsections)]
+                                              [rc/box
+                                               :attr {:key (sub-key header sub)}
+                                               :child [:a
+                                                       {:data-dispatch (:data-dispatch sub)
+                                                        :on-click      (fn []
+                                                                         (swap! s assoc-in [:last-sub] (sub-key header j))
+                                                                         (rf/dispatch [(:data-dispatch sub)])
+                                                                         (rf/dispatch [dispatch-target :hide]))}
+                                                       [:div {:class (when (= (:last-sub @s) (sub-key header j))
+                                                                       :accordeon-menu-sub-selected)}
+                                                        (:content sub)]]]))]]]))
+                    [gap]
+                    [rc/box
+                     :class "webui-footer"
+                     :child "Footer"]]]))))
 
-
-;(defn accordeon-menu-ctrl [component-name]
-;  (let [dispatch-target (display-dispatch-target-name component-name)]
-;    [:div.accordeon-menu-ctrl
-;    [:a {:on-click #(let [display @(rf/subscribe [dispatch-target])]
-;                      (rf/dispatch [dispatch-target (if (= :show display) :hide :show)]))}
-;     [:i.material-icons "menu"]]]))
-
+; For doc...
 ;(defn main-panel []
 ;  (fn []
 ;    [:div#main
