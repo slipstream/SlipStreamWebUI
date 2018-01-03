29af2406c47ac7f760514e9b10504e8d23c9ce8d
khaled basbous <khaled@sixsq.com>
2018-01-03 19:09:40 +0100

    enhance test still issue on centos for tests

--- test/cljs/sixsq/slipstream/webui/runner.cljs
+++ test/cljs/sixsq/slipstream/webui/runner.cljs
@@ -1,8 +1,12 @@
 (ns sixsq.slipstream.webui.runner
   (:require [cljs.test :as test]
-            [doo.runner :refer-macros [doo-all-tests]]
+            [doo.runner :refer-macros [doo-tests]]
             [sixsq.slipstream.webui.utils-test]
             [sixsq.slipstream.webui.panel.cimi.utils-test]
             [sixsq.slipstream.webui.widget.breadcrumbs.utils-test]))
 
-(doo-all-tests)
+(doo-tests
+  'sixsq.slipstream.webui.utils-test
+  'sixsq.slipstream.webui.panel.cimi.utils-test
+  'sixsq.slipstream.webui.widget.breadcrumbs.utils-test
+  )
