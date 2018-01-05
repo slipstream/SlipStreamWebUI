eaf85b1a78aebd6578b2518df40842b97df6d28d
khaled basbous <khaled@sixsq.com>
2018-01-05 19:51:22 +0100

    fix issue with trash migrating from one line to another because of component state

--- src/cljs/sixsq/slipstream/legacy/components.cljs
+++ src/cljs/sixsq/slipstream/legacy/components.cljs
@@ -1,13 +1,15 @@
 (ns sixsq.slipstream.legacy.components
   (:require [sixsq.slipstream.legacy.components.dashboard-tabs :as dashboard-tabs]
             [sixsq.slipstream.legacy.components.dashboard-tabs.deployments :as deployments]
-            [sixsq.slipstream.legacy.components.metering :as metering]))
+            [sixsq.slipstream.legacy.components.metering :as metering]
+            [taoensso.timbre :as log]))
 
 (defn render-component-when-present [tag component-init-fn]
-      (when-let [container-element (.getElementById js/document tag)]
-                (component-init-fn container-element)))
+  (when-let [container-element (.getElementById js/document tag)]
+    (component-init-fn container-element)))
 
 (defn ^:export init []
-      (render-component-when-present "dashboard-tabs-container" dashboard-tabs/init)
-      (render-component-when-present "metering-container" metering/init)
-      (render-component-when-present "deployments-container" deployments/init))
+  (log/set-level! :info)
+  (render-component-when-present "dashboard-tabs-container" dashboard-tabs/init)
+  (render-component-when-present "metering-container" metering/init)
+  (render-component-when-present "deployments-container" deployments/init))
