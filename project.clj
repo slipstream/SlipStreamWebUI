35b6737edcc12b5d4988a850a7dd52007eb77cbc
Charles Loomis <cal@sixsq.com>
2018-02-04 06:28:44 +0100

    revert v3.45 release commits

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(def +version+ "3.46-SNAPSHOT")
+(def +version+ "3.45-SNAPSHOT")
 
-(defproject com.sixsq.slipstream/SlipStreamWebUI "3.46-SNAPSHOT"
+(defproject com.sixsq.slipstream/SlipStreamWebUI "3.45-SNAPSHOT"
 
   :description "Web Browser User Interface"
 
@@ -18,7 +18,7 @@
             [pdok/lein-filegen "0.1.0"]
             [lein-resource "16.9.1"]]
 
-  :parent-project {:coords  [com.sixsq.slipstream/parent "3.46-SNAPSHOT"]
+  :parent-project {:coords  [com.sixsq.slipstream/parent "3.45-SNAPSHOT"]
                    :inherit [:min-lein-version
                              :managed-dependencies
                              :repositories
