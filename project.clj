e55e3987d509664c0a3e09c69faa6b08846e8e0a
Charles Loomis <cal@sixsq.com>
2018-03-03 17:22:22 +0100

    change lein parent

--- project.clj
+++ project.clj
@@ -18,7 +18,7 @@
             [pdok/lein-filegen "0.1.0"]
             [lein-resource "16.9.1"]]
 
-  :parent-project {:coords  [com.sixsq.slipstream/parent "3.47-SNAPSHOT"]
+  :parent-project {:coords  [sixsq/slipstream-parent "4.0.0"]
                    :inherit [:min-lein-version
                              :managed-dependencies
                              :repositories
