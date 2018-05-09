9a8f8cd06a7f226af3f2d4569bc319ea20f243eb
Charles Loomis <cal@sixsq.com>
2018-05-09 10:54:45 +0200

    update parent; fix deps

--- project.clj
+++ project.clj
@@ -32,7 +32,7 @@
             [pdok/lein-filegen "0.1.0"]
             [lein-resource "16.9.1"]]
 
-  :parent-project {:coords  [sixsq/slipstream-parent "5.3.3"]
+  :parent-project {:coords  [sixsq/slipstream-parent "5.3.5"]
                    :inherit [:plugins
                              :min-lein-version
                              :managed-dependencies
