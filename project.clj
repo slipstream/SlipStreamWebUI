6f2d8457ef6bb685d10941244605571b9602c693
sixsq-hudson <hudson@sixsq.com>
2018-05-17 18:11:13 +0000

    release v3.51

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(def +version+ "3.51-SNAPSHOT")
+(def +version+ "3.51")
 
 (def package-json-template
   "
@@ -14,7 +14,7 @@
   }
   ")
 
-(defproject com.sixsq.slipstream/SlipStreamWebUI "3.51-SNAPSHOT"
+(defproject com.sixsq.slipstream/SlipStreamWebUI "3.51"
 
   :description "Web Browser User Interface"
 
