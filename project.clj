8eece0009cba33ffa069a82afbc456ffcd436fbe
sixsq-hudson <hudson@sixsq.com>
2018-09-14 13:20:52 +0000

    next development version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(def +version+ "3.59")
+(def +version+ "3.60-SNAPSHOT")
 
 (def package-json-template
   "
@@ -14,7 +14,7 @@
   }
   ")
 
-(defproject com.sixsq.slipstream/SlipStreamWebUI "3.59"
+(defproject com.sixsq.slipstream/SlipStreamWebUI "3.60-SNAPSHOT"
 
   :description "Web Browser User Interface"
 
