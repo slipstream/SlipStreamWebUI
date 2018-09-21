7010ae0877cc1aa41a9e82eac7f875514d74b466
Khaled Basbous <khaled@sixsq.com>
2018-09-21 13:58:40 +0200

    paging added for nuvlaboxes (#263)

--- src/cljs/sixsq/slipstream/webui/nuvlabox/views.cljs
+++ src/cljs/sixsq/slipstream/webui/nuvlabox/views.cljs
@@ -6,21 +6,17 @@
     [sixsq.slipstream.webui.history.events :as history-events]
     [sixsq.slipstream.webui.i18n.subs :as i18n-subs]
     [sixsq.slipstream.webui.main.subs :as main-subs]
-    [sixsq.slipstream.webui.messages.events :as messages-events]
     [sixsq.slipstream.webui.nuvlabox-detail.events :as nuvlabox-detail-events]
     [sixsq.slipstream.webui.nuvlabox-detail.views :as nuvlabox-detail]
     [sixsq.slipstream.webui.nuvlabox.events :as nuvlabox-events]
     [sixsq.slipstream.webui.nuvlabox.subs :as nuvlabox-subs]
-    [sixsq.slipstream.webui.main.events :as main-events]
     [sixsq.slipstream.webui.panel :as panel]
     [sixsq.slipstream.webui.utils.forms :as forms]
-    [sixsq.slipstream.webui.utils.response :as response]
     [sixsq.slipstream.webui.utils.semantic-ui :as ui]
     [sixsq.slipstream.webui.utils.semantic-ui-extensions :as uix]
     [sixsq.slipstream.webui.utils.style :as style]
     [sixsq.slipstream.webui.utils.ui-callback :as ui-callback]
-    [taoensso.timbre :as log]
-    [reagent.core :as reagent]))
+    [sixsq.slipstream.webui.utils.general :as general-utils]))
 
 
 (defn stat
@@ -32,10 +28,10 @@
 
 (defn health-summary
   []
-  (let [results (subscribe [::nuvlabox-subs/collection])
+  (let [nuvlabox-records (subscribe [::nuvlabox-subs/nuvlabox-records])
         health-info (subscribe [::nuvlabox-subs/health-info])]
     (let [{:keys [stale-count active-count]} @health-info
-          total (some->> @results :nuvlaboxRecords count)
+          total (get @nuvlabox-records :count 0)
           unknown (max (- total stale-count active-count) 0)]
       [ui/Segment style/evenly
        [stat total "computer" "black" "NuvlaBox"]
@@ -45,45 +41,24 @@
 
 
 (defn search-header []
-  (let [tr (subscribe [::i18n-subs/tr])
-        query-params (subscribe [::nuvlabox-subs/query-params])
-        state-selector (subscribe [::nuvlabox-subs/state-selector])]
+  (let [state-selector (subscribe [::nuvlabox-subs/state-selector])]
     (fn []
       ;; reset visible values of parameters
-      (let [{:keys [$last $select]} @query-params]
-        [ui/Form {:on-key-press
-                  (partial forms/on-return-key
-                           (fn []
-                             (dispatch [::nuvlabox-events/fetch-health-info])
-                             (dispatch [::nuvlabox-events/get-results])))}
+      [ui/Form {:on-key-press (partial forms/on-return-key #(dispatch [::nuvlabox-events/get-nuvlabox-records]))}
 
-         [ui/FormGroup
+       [ui/FormGroup
 
-          [ui/FormField
-           ; the key below is a workaround react issue with controlled input cursor position,
-           ; this will force to re-render defaultValue on change of the value
-           ^{:key (str "last:" $last)}
-           [ui/Input {:type         "number"
-                      :min          0
-                      :label        (@tr [:last])
-                      :defaultValue $last
-                      :on-blur      (ui-callback/input-callback
-                                      #(dispatch [::nuvlabox-events/set-last %]))}]]
-
-          [ui/FormField
-           ^{:key (str "state:" @state-selector)}
-           [ui/Dropdown
-            {:value     @state-selector
-             :scrolling false
-             :selection true
-             :options   [{:value "all", :text "all states"}
-                         {:value "new", :text "new state"}
-                         {:value "activated", :text "activated state"}
-                         {:value "quarantined", :text "quarantined state"}]
-             :on-change (ui-callback/value (fn [value]
-                                             (dispatch [::nuvlabox-events/set-state-selector value])
-                                             (dispatch [::nuvlabox-events/fetch-health-info])
-                                             (dispatch [::nuvlabox-events/get-results])))}]]]]))))
+        [ui/FormField
+         ^{:key (str "state:" @state-selector)}
+         [ui/Dropdown
+          {:value     @state-selector
+           :scrolling false
+           :selection true
+           :options   [{:value "all", :text "all states"}
+                       {:value "new", :text "new state"}
+                       {:value "activated", :text "activated state"}
+                       {:value "quarantined", :text "quarantined state"}]
+           :on-change (ui-callback/value #(dispatch [::nuvlabox-events/set-state-selector %]))}]]]])))
 
 
 (defn search-button
@@ -98,26 +73,16 @@
         :disabled  false #_(nil? @selected-id)
         :on-click  (fn []
                      (dispatch [::nuvlabox-events/fetch-health-info])
-                     (dispatch [::nuvlabox-events/get-results]))}])))
+                     (dispatch [::nuvlabox-events/get-nuvlabox-records]))}])))
 
 
 (defn menu-bar []
-  (let [tr (subscribe [::i18n-subs/tr])
-        resources (subscribe [::nuvlabox-subs/collection])]
-    (fn []
-      (when (instance? js/Error @resources)
-        (dispatch [::messages-events/add
-                   (let [{:keys [status message]} (response/parse-ex-info @resources)]
-                     {:header  (cond-> (@tr [:error])
-                                       status (str " (" status ")"))
-                      :message message
-                      :type    :error})]))
-      [ui/Segment style/basic
-       [ui/Menu {:attached   "top"
-                 :borderless true}
-        [search-button]]
-       [ui/Segment {:attached "bottom"}
-        [search-header]]])))
+  [ui/Segment style/basic
+   [ui/Menu {:attached   "top"
+             :borderless true}
+    [search-button]]
+   [ui/Segment {:attached "bottom"}
+    [search-header]]])
 
 
 (defn format-nb-header
@@ -154,31 +119,26 @@
 
 (defn nb-table
   []
-  (let [results (subscribe [::nuvlabox-subs/collection])
+  (let [nuvlabox-records (subscribe [::nuvlabox-subs/nuvlabox-records])
+        elements-per-page (subscribe [::nuvlabox-subs/elements-per-page])
+        page (subscribe [::nuvlabox-subs/page])
         health-info (subscribe [::nuvlabox-subs/health-info])]
-    (dispatch [::nuvlabox-events/get-results])
+    (dispatch [::nuvlabox-events/get-nuvlabox-records])
     (fn []
       (let [{:keys [healthy?]} @health-info
-            data (some->> @results
-                          :nuvlaboxRecords
-                          (map #(select-keys % #{:id :macAddress :state :name})))]
+            total-elements (get @nuvlabox-records :count 0)]
         [:div
+
          [ui/Table {:compact "very", :selectable true}
           (format-nb-header)
           (vec (concat [ui/TableBody]
-                       (mapv (partial format-nb-row healthy?) data)))]
+                       (mapv (partial format-nb-row healthy?) (get @nuvlabox-records :nuvlaboxRecords []))))]
+
+         [uix/Pagination {:size         "tiny"
+                          :totalPages   (general-utils/total-pages total-elements @elements-per-page)
+                          :activePage   @page
+                          :onPageChange (ui-callback/callback :activePage #(dispatch [::nuvlabox-events/set-page %]))}]
          ]))))
-
-
-(defn general-view []
-  (let []
-    (dispatch [::main-events/action-interval {:action    :start
-                                              :id        :nuvlabox-health-info
-                                              :frequency 15000
-                                              :event     [::nuvlabox-events/fetch-health-info]}])
-    [[menu-bar]
-     [health-summary]
-     [nb-table]]))
 
 
 (defn nb-info
@@ -188,9 +148,13 @@
       (let [[_ mac] @path
             n (count @path)
             children (case n
-                       1 (general-view)
+                       1 [[menu-bar]
+                          [health-summary]
+                          [nb-table]]
                        2 [[nuvlabox-detail/nb-detail]]
-                       (general-view))]
+                       [[menu-bar]
+                        [health-summary]
+                        [nb-table]])]
         (dispatch [::nuvlabox-detail-events/set-mac mac])
         (vec (concat [ui/Segment style/basic] children))))))
 
