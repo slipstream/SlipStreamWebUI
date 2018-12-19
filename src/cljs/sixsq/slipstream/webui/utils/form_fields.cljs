53df39a438823fb21630199c69134d467ae4af48
khaled basbous <khaled@sixsq.com>
2018-12-19 15:16:30 +0100

    add some form-field basic support. to be completed and extended

--- src/cljs/sixsq/slipstream/webui/utils/form_fields.cljs
+++ src/cljs/sixsq/slipstream/webui/utils/form_fields.cljs
@@ -3,7 +3,8 @@
     [reagent.core :as reagent]
     [sixsq.slipstream.webui.utils.semantic-ui :as ui]
     [sixsq.slipstream.webui.utils.ui-callback :as ui-callback]
-    [taoensso.timbre :as log]))
+    [taoensso.timbre :as log]
+    [sixsq.slipstream.webui.utils.general :as utils]))
 
 
 (def nbsp "\u00a0")
@@ -39,45 +40,49 @@
             hidden (assoc :style {:display "none"}))]])
 
 
-;(defmethod form-field :int
-;  [update-fn form-id [param-name {:keys [data displayName readOnly mandatory description autocomplete]}]]
-;  ^{:key displayName}
-;  [ui/FormField {:required mandatory}
-;   [:label displayName nbsp (help-popup description)]
-;   [ui/Input
-;    {:type          "number"
-;     :name          param-name
-;     :default-value (or data "")
-;     :read-only     readOnly
-;     :fluid         true
-;     :autoComplete  (or autocomplete "off")
-;     :on-change     (ui-callback/value #(update-fn form-id param-name (utils/str->int %)))}]])
-;
-;
-;(defmethod form-field :boolean
-;  [update-fn form-id [param-name {:keys [data displayName readOnly mandatory description]}]]
-;  ^{:key displayName}
-;  [ui/FormField {:required mandatory}
-;   [:label displayName nbsp (help-popup description)]
-;   [ui/Checkbox
-;    {:default-value false
-;     :read-only     readOnly
-;     :on-change     (ui-callback/checked #(update-fn form-id param-name %))}]])
-;
-;(defmethod form-field :href
-;  [update-fn form-id [param-name {:keys [data displayName readOnly mandatory description autocomplete] :as param}]]
-;  ^{:key displayName}
-;  [ui/FormField {:required mandatory}
-;   [:label displayName nbsp (help-popup description)]
-;   [ui/Input
-;    {:type          "text"
-;     :name          param-name
-;     :default-value (or (:href data) "")
-;     :read-only     readOnly
-;     :fluid         true
-;     :autoComplete  (or autocomplete "off")
-;     :on-change     (ui-callback/value #(update-fn form-id param-name {:href %}))}]])
-;
+(defmethod form-field :integer
+  [update-fn form-id {:keys [name displayName help hidden vscope
+                             consumerMandatory consumerWritable] :as attribute}]
+  ^{:key name}
+  [ui/FormField {:required consumerMandatory}
+   (when-not hidden [:label displayName nbsp (help-popup help)])
+   [ui/Input
+    (cond-> {:type          "number"
+             :name          (or displayName name)
+             :default-value (or (:value vscope) (:default vscope) "")
+             :read-only     (not consumerWritable)
+             :on-change     (ui-callback/value #(update-fn form-id name (utils/str->int %)))}
+            hidden (assoc :style {:display "none"}))]])
+
+
+(defmethod form-field :boolean
+  [update-fn form-id {:keys [name displayName help hidden vscope
+                             consumerMandatory consumerWritable] :as attribute}]
+  ^{:key name}
+  [ui/FormField {:required consumerMandatory}
+   (when-not hidden [:label displayName nbsp (help-popup help)])
+   [ui/Checkbox
+    (cond-> {:type          "number"
+             :name          (or displayName name)
+             :default-value (or (:value vscope) (:default vscope) false)
+             :read-only     (not consumerWritable)
+             :on-change     (ui-callback/checked #(update-fn form-id name %))}
+            hidden (assoc :style {:display "none"}))]])
+
+
+(defmethod form-field :ref
+  [update-fn form-id {:keys [name displayName help hidden vscope
+                             consumerMandatory consumerWritable] :as attribute}]
+  ^{:key name}
+  [ui/FormField {:required consumerMandatory}
+   (when-not hidden [:label displayName nbsp (help-popup help)])
+   [ui/Checkbox
+    (cond-> {:name          (or displayName name)
+             :default-value (or (:value vscope) (:default vscope) "")
+             :read-only     (not consumerWritable)
+             :on-change     (ui-callback/value #(update-fn form-id name {:href %}))}
+            hidden (assoc :style {:display "none"}))]])
+
 ;
 ;(defmethod form-field :enum
 ;  [update-fn form-id [param-name {:keys [enum data displayName mandatory description]}]]
@@ -88,3 +93,5 @@
 ;    {:options   (mapv (fn [v] {:value v, :text v}) enum)
 ;     :value     data
 ;     :on-change (ui-callback/value #(update-fn form-id param-name %))}]])
+
+; add support for following types map, array, dateTime
