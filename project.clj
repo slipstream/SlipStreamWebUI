c7e39d009fcb4c9da2ff63c6527c1e1521b2e202
sixsq-hudson <hudson@sixsq.com>
2018-12-21 10:49:41 +0000

    release v3.66

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(def +version+ "3.66-SNAPSHOT")
+(def +version+ "3.66")
 
-(defproject com.sixsq.slipstream/SlipStreamWebUI "3.66-SNAPSHOT"
+(defproject com.sixsq.slipstream/SlipStreamWebUI "3.66"
 
   :description "Web Browser User Interface"
 
